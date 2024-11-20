81c0fc2509f7dfb5e871c65009d6f09c2c7005a3
jhonny1997str <167597745+jhonny1997str@users.noreply.github.com>
2024-11-20 10:58:28 -0500

    Done

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,12 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('CustomerController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  let createdCustomerId: number; // Variable para almacenar el ID del usuario creado
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +18,44 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  beforeEach(async () => {
+    // Crear un usuario antes de cada prueba
+    const response = await request(app.getHttpServer())
+      .post('/customers')
+      .send({
+        customerName: 'Carlos',
+        email: 'carlos@gmail.com',
+      })
+      .expect(201);
+
+    // Almacenar el ID del usuario creado
+    createdCustomerId = response.body.customerId;
   });
+
+  afterEach(async () => {
+    // Eliminar el usuario después de cada prueba
+    await request(app.getHttpServer())
+      .delete(`/customers/${createdCustomerId}`)
+      .expect(200);
+  });
+
+  afterAll(async () => {
+    // Cerrar la aplicación al final de todas las pruebas
+    await app.close();
+  });
+
+  it('/customers (GET)', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/customers')
+      .expect(200);
+
+    // Verificar que el usuario creado esté en la lista
+    expect(response.body).toContainEqual({
+      customerId: createdCustomerId,
+      customerName: 'Carlos',
+      email: 'carlos@gmail.com',
+    });
+  });
+  
+
 });
